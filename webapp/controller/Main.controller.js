e2128796829c14a1a3ea5eace8d74ebf89735144
hyun-jung-joo <wnguswjd0418@naver.com>
2025-03-11 22:41:39 +0900

    [Fix] Main을 기본 화면으로

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,10 +1,28 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+sap.ui.define(["sap/ui/core/mvc/Controller"], (Controller) => {
     "use strict";
 
     return Controller.extend("sync.c22.salesreport.controller.Main", {
-        onInit() {
-        }
+        onInit() {},
+        // item 클릭 되었을 때
+        onPressItemClick(oEvent) {
+            // 클릭한 행 정보
+            var oItem = oEvent.getSource();
+
+            // sales 모델 가져오기
+            var oContext = oItem.getBindingContext("sales");
+
+            // Main.view.xml
+            var oView = this.getView();
+
+            // SalesDetail.view.xml
+            var oDetailView = oView.byId("idDetailView");
+
+            // Detail 에다가 현재의 상품 경로를 sales 모델과 연결해준다.
+            var sPath = oContext.getPath();
+            oDetailView.bindElement({
+                path: sPath,
+                model: "sales",
+            });
+        },
     });
 });
