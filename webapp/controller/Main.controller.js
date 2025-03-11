130e5ffdb6c83f4a47a55e37bf9fde97de66b2e0
hyun-jung-joo <wnguswjd0418@naver.com>
2025-03-11 23:05:38 +0900

    [Feat] Dialog

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,28 +1,61 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], (Controller) => {
-    "use strict";
+sap.ui.define(
+    ["sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel"],
+    (Controller, JSONModel) => {
+        "use strict";
 
-    return Controller.extend("sync.c22.salesreport.controller.Main", {
-        onInit() {},
-        // item 클릭 되었을 때
-        onPressItemClick(oEvent) {
-            // 클릭한 행 정보
-            var oItem = oEvent.getSource();
+        return Controller.extend("sync.c22.salesreport.controller.Main", {
+            onInit() {
+                var oModel = new JSONModel({
+                    Unit: "EA",
+                });
+                // view에 "view"라는 이름으로 모델 정의
+                // view>/Unit
+                this.getView().setModel(oModel, "view");
+            },
 
-            // sales 모델 가져오기
-            var oContext = oItem.getBindingContext("sales");
+            // item 클릭 되었을 때
+            // 1. detail view 에 정보 binding
+            // 2. 정보 fragment 띄우기
+            async onPressItemClick(oEvent) {
+                // 클릭한 행 정보
+                var oItem = oEvent.getSource();
 
-            // Main.view.xml
-            var oView = this.getView();
+                // sales 모델 가져오기
+                var oContext = oItem.getBindingContext("sales");
 
-            // SalesDetail.view.xml
-            var oDetailView = oView.byId("idDetailView");
+                // Main.view.xml
+                var oView = this.getView();
 
-            // Detail 에다가 현재의 상품 경로를 sales 모델과 연결해준다.
-            var sPath = oContext.getPath();
-            oDetailView.bindElement({
-                path: sPath,
-                model: "sales",
-            });
-        },
-    });
-});
+                // SalesDetail.view.xml
+                var oDetailView = oView.byId("idDetailView");
+
+                // Detail 에다가 현재의 상품 경로를 sales 모델과 연결해준다.
+                var sPath = oContext.getPath();
+                oDetailView.bindElement({
+                    path: sPath,
+                    model: "sales",
+                });
+
+                // fragment load (비동기)
+                this.oDialog ??= await this.loadFragment({
+                    name: "sync.c22.salesreport.view.Detail",
+                });
+
+                // load 해온 fragment 에 bindElement
+                this.oDialog.bindElement({
+                    path: sPath,
+                    model: "sales",
+                });
+                this.oDialog.open(); // fragment open
+            },
+
+            // dialog 닫기 구현
+            onCloseDialog() {
+                var oDialog = this.byId("idDialog");
+                if (oDialog) {
+                    oDialog.close();
+                }
+            },
+        });
+    }
+);
